d34ae8e5504aa79f9d8075c58592d3304db3093a
radibarq <radibaraq@gmail.com>
2026-05-17 18:47:35 +0300

    Search screen UI polish

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,7 +1,30 @@
 import { View, Image, Text, StyleSheet } from 'react-native';
 
-const ResultsDetail = () => {
-  return <View>
-    <Text></Text>
-  </View>;
+const ResultsDetail = ({ result }) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count}
+      </Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15,
+  },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+    marginBottom: 5,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+});
+
+export default ResultsDetail;
